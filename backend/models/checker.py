5c84dc0e421c49c387f3b27122e4ab612b596002
daveyhmariam <daveyhmariam@gmail.com>
2024-09-19 10:12:58 +0300

    Add FileChecker class with repository cloning and file validation
    - Created  class extending  to manage repository cloning and file existence checks.
    - Implemented methods:
      - : Clones or updates a Git repository based on the provided URL and branch.
      - : Validates the existence of a file in the cloned repository, updating the score accordingly.
      - : Runs the full execution pipeline for repository cloning and file checking.
    - Added test case in the main section to demonstrate repository cloning and file validation, printing the results.

--- backend/models/checker.py
+++ backend/models/checker.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 import uuid
+from backend import models
+from bson import ObjectId
 import json
 
 
 class Checker():
     """Base checker class
     """
-    def __init__(self, name, type, dir, file_name, weight, clone_dir, **kwargs):
-        print(kwargs)
-            
-        if "_id" in kwargs.keys():
-            self._id = kwargs['_id']
-        else:
-            self._id = str(uuid.uuid4())
-            print(f'in else {self._id}')
+
+    def __init__(self, name, type, dir,
+                 file_name, weight,
+                 clone_dir, **kwargs):
+
+        self._id = kwargs.get('_id', str(ObjectId()))
 
         self.name = name
         self.type = type
@@ -26,14 +26,10 @@ class Checker():
         self.status = 'INCOMPLETE'
 
         if kwargs:
-            print('\n\n\nin kwargs')
-            print(kwargs)
             for key, value in kwargs.items():
-                print(f"{key}__{value}\n")
                 if key != "__class__":
                     setattr(self, key, value)
-
-
+        models.storage.new(self)
 
     def to_dict(self):
         new_dict = dict()
@@ -43,16 +39,18 @@ class Checker():
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
 
+
 if __name__ == "__main__":
     repo_url = 'https://github.com/daveyhmariam/alx-low_level_programming.git'
     file_name = '0x14-bit_manipulation/0-binary_to_uint.c'  # Example file name
-    clone_dir = '/home/falcon/alx_2/ByteSchool/e6fedb62-7a84-4ef0-ab43-48aea3b04daf/' + repo_url.split('/')[-1].split('.')[0]
+    clone_dir = '/home/falcon/alx_2/ByteSchool/e6fedb62-7a84-4ef0-ab43-48aea3b04daf/' + \
+        repo_url.split('/')[-1].split('.')[0]
     type = 'file_checker'
     dir = '0x14-bit_manipulation'
-    args = {'_id': 'e2901908-957c-4b7d-a332-bc193cf3be76', 'name':'FileChecker', 'type':type, 'dir':dir, 'file_name':file_name, 'weight':2, 'clone_dir':clone_dir}
-    checker = Checker(**args)
+    args = {'name': 'FileChecker',
+            'type': type, 'dir': dir, 'file_name': file_name, 'weight': 2, 'clone_dir': clone_dir}
+    checker = Checker(_id='e2901908-957c-4b7d', **args)
     print(checker.__dict__)
-    #print(f"Final score: {checker.file_name}")
-    
-    print(f"Final score: {checker.__dict__}")
+    # print(f"Final score: {checker.file_name}")
 
+    print(f"Final score: {checker.__dict__}")
--- backend/models/file_checker.py
+++ backend/models/file_checker.py
@@ -4,12 +4,19 @@ from backend.models.checker import Checker
 import os
 import subprocess
 from git import Repo
+from backend import models
+
 
 class FileChecker(Checker):
-    def __init__(self, name: str, type, dir,  file_name: str, weight, clone_dir, repo_url, branch: str = 'main', **kwargs):
+    def __init__(self, name: str, type,
+                 dir,  file_name: str,
+                 weight, clone_dir,
+                 repo_url, branch: str = 'main',
+                 **kwargs):
 
-
-        super().__init__(name=name, type=type, dir=dir, file_name=file_name, weight=weight, clone_dir=clone_dir)
+        super().__init__(name=name, type=type, dir=dir,
+                         file_name=file_name, weight=weight,
+                         clone_dir=clone_dir)
         self.repo_url = repo_url
         self.branch = branch
 
@@ -17,6 +24,8 @@ class FileChecker(Checker):
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
+        models.storage.new(self)
+
 
 
     def clone_repo(self):
@@ -24,21 +33,21 @@ class FileChecker(Checker):
         if os.path.exists(self.clone_dir):
             print(f"Directory '{self.clone_dir}' already exists. Updating it.")
             try:
-                #subprocess.run(['rm', '-rf', self.clone_dir], check=True)
+                # subprocess.run(['rm', '-rf', self.clone_dir], check=True)
                 print("updating")
                 repo = Repo(self.clone_dir)
-                #repo.remotes.origin.fetch()
-                #repo.remotes.origin.pull(self.branch)
+                repo.remotes.origin.fetch()
+                repo.remotes.origin.pull(self.branch)
                 pass
             except subprocess.CalledProcessError as e:
                 print(f"Error removing directory: {e}")
                 return
-        
+
         else:
             print(f"Cloning repository from {self.repo_url}...")
             try:
                 print(f"clone_dir: {self.clone_dir}")
-                #Repo.clone_from(self.repo_url, self.clone_dir, branch=self.branch)
+                Repo.clone_from(self.repo_url, self.clone_dir, branch=self.branch)
                 print("cloning done")
                 pass
             except Exception as e:
@@ -53,10 +62,12 @@ class FileChecker(Checker):
             self.score = self.weight
             print(f"File '{self.file_name}' exists. Score: {self.score}")
             self.status = "COMPLETE"
+            models.storage.save_object(self)
             return full_path
         else:
             self.score = 0
-            print(f"File '{self.file_name}' does not exist. Score: {self.score}")
+            print(
+                f"File '{self.file_name}' does not exist. Score: {self.score}")
 
     def execute(self):
         """ Full execution pipeline. """
@@ -65,14 +76,17 @@ class FileChecker(Checker):
         full_path = self.check_exist()
         return full_path
 
+
 if __name__ == '__main__':
     repo_url = 'https://github.com/daveyhmariam/alx-low_level_programming.git'
     file_name = '0-binary_to_uint.c'  # Example file name
-    clone_dir = '/home/falcon/alx_2/ByteSchool/e6fedb62-7a84-4ef0-ab43-48aea3b04daf/' + repo_url.split('/')[-1].split('.')[0]
+    clone_dir = '/home/falcon/alx_2/ByteSchool/e6fedb62-7a84-4ef0-ab43-48aea3b04daf/' + \
+        repo_url.split('/')[-1].split('.')[0]
     type = 'file_checker'
     dir = '0x14-bit_manipulation'
-    args = {'name':'FileChecker', 'type':type, 'dir':dir, 'file_name':file_name, 'weight':1, 'clone_dir':clone_dir, 'repo_url':repo_url}
-    checker = FileChecker('FileChecker', type, dir, file_name, 1, clone_dir, repo_url, "main")
+    args = {"_id": "my_id", 'name': 'FileChecker', 'type': type, 'dir': dir,
+            'file_name': file_name, 'weight': 1, 'clone_dir': clone_dir, 'repo_url': repo_url, 'branch': "main"}
+    checker = FileChecker(**args)
     print(checker.__dict__)
     print(checker.execute())
     print(f"Final score: {checker.score}")
